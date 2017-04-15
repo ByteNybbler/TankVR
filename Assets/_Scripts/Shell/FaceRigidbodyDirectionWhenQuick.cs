ce1f7196824fcfbddb5c04c7989bf577204ba880
Paul Calande <paul.luap09@gmail.com>
2017-04-15 09:33:24 -0400

    autism cure

---
+++ Assets/_Scripts/Shell/FaceRigidbodyDirectionWhenQuick.cs
@@ -0,0 +1,28 @@
+﻿// Author(s): Paul Calande
+// Make the object face the direction of the rigidbody's velocity... but only at a certain speed.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceRigidbodyDirectionWhenQuick : MonoBehaviour
+{
+    [Tooltip("How fast the rigidbody must be for the correction to take effect.")]
+    public float requiredSpeed;
+
+    // Component references.
+    private Rigidbody compRigidbody;
+
+    private void Awake()
+    {
+        compRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (compRigidbody.velocity != Vector3.zero && compRigidbody.velocity.magnitude > requiredSpeed)
+        {
+            transform.rotation = Quaternion.LookRotation(compRigidbody.velocity);
+        }
+    }
+}
