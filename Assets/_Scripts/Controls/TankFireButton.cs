a250c8c00c594b6352294882c369e5d0f4f4af34
Paul Calande <paul.luap09@gmail.com>
2017-04-15 06:02:33 -0400

    hit apply on some prefabs, add some tags

---
+++ Assets/_Scripts/Controls/TankFireButton.cs
@@ -0,0 +1,33 @@
+﻿// Author(s): Paul Calande
+// Button class.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankFireButton : MonoBehaviour
+{
+    [Tooltip("Button sound for failure to fire shell.")]
+    public AudioClip soundFailure;
+
+    // Component references.
+    private AudioSource compAudio;
+
+    private void Awake()
+    {
+        compAudio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Controller")
+        {
+            Press();
+        }
+    }
+
+    private void Press()
+    {
+        compAudio.PlayOneShot(soundFailure);
+    }
+}
