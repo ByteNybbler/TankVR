b2ee44bec0018f06d8ac5d567ef8cd4ab04f33f4
Paul Calande <paul.luap09@gmail.com>
2017-04-14 20:49:31 -0400

    PROJECTILES ARE IN BOY

---
+++ Assets/_Scripts/ShellShooter.cs
@@ -0,0 +1,37 @@
+﻿// Author(s): Paul Calande
+// Class that shoots shells.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellShooter : MonoBehaviour
+{
+    [Tooltip("Shell prefab.")]
+    public GameObject prefabShell;
+    [Tooltip("The force at which the shell is launched.")]
+    public float launchForce;
+    [Tooltip("Number of seconds between each shot.")]
+    public float fireRate;
+
+    public void FireShell()
+    {
+        GameObject shell = Instantiate(prefabShell, transform.position, transform.rotation);
+        Rigidbody rb = shell.GetComponent<Rigidbody>();
+        rb.AddForce(transform.rotation * Vector3.forward * launchForce, ForceMode.Impulse);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(ShellSpam());
+    }
+
+    IEnumerator ShellSpam()
+    {
+        while (true)
+        {
+            FireShell();
+            yield return new WaitForSeconds(fireRate);
+        }
+    }
+}
